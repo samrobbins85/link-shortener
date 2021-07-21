bae135ff82ec84bee30bb0018ed0ee75a23c422e
Sam Robbins <samrobbinsgb@gmail.com>
2021-07-21 10:45:35 +0100

    Even more api testing

--- pages/api/[...slug].js
+++ pages/api/[...slug].js
@@ -16,6 +16,6 @@ export default async function handler(req, res) {
   if (link.length > 0) {
     res.redirect(link[0].link);
   } else {
-    res.status(404).json(data);
+    res.status(200).json(data);
   }
 }
